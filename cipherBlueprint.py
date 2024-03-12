aab7882f5d90eca7ab805876307772af0fb0ccb6
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-03-12 21:04:37 +0000

    Create cipherBlueprint.py
    Use this to build your solutions to ciphers.

---
+++ cipherBlueprint.py
@@ -0,0 +1,21 @@
+import random
+
+class nameOfCipherInCamelCase(): # also name the file the same way please
+
+    def __init__(self,cipher):
+        self.__cipher = cipher # the cipherText
+        self.__key = "any valid key or group of keys, you can split this up into multiple variables if you need to"
+
+    def shuffle(self):
+        # code to randomly shuffle the key (the shuffle should be the smallest shuffle possible such that the plain text
+        # deciphered is as similar to the previous as possible)
+        pass
+
+    def undoShuffle(self):
+        # only needs to undo one previous shuffle. This function will not be called twice in a row, only ever with a
+        # shuffle() in between
+        pass
+
+    def decipher(self):
+        # code to get plainText from self.__cipher using self.__key (or whatever keys you're using)
+        return plainText
