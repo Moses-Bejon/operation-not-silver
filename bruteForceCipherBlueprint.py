6f40d216b3610c2ad6a70f47161318cf4c538a5a
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-03-31 00:18:10 +0000

    Create bruteForceCipherBlueprint.py

---
+++ bruteForceCipherBlueprint.py
@@ -0,0 +1,11 @@
+class nameOfCipherInCamelCase(): # also name the file the same way please
+
+    def __init__(self,cipher):
+        self.__cipher = cipher # the cipherText
+        
+        # do any computation you need to do beforehand here
+
+    def plainTexts(self):
+        for keys in listOfKeys:
+            # calculate plain text
+            yield plainText
