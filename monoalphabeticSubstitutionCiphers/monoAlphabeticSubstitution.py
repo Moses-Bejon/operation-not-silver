51febe97fbc6e79c854f3e454cb72539be50bfcf
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-05-18 10:51:50 +0100

    Refactored monoAlphabeticSubstitution.py
    now handles integer arrays rather than strings

--- monoalphabeticSubstitutionCiphers/monoAlphabeticSubstitution.py
+++ monoalphabeticSubstitutionCiphers/monoAlphabeticSubstitution.py
@@ -1,13 +1,13 @@
 import random
-
 class monoAlphabeticSubstitution():
-  
     def __init__(self,cipher):
         self.__cipher = cipher
-        self.__key = {'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e', 'f': 'f', 'g': 'g', 'h': 'h', 'i': 'i', 'j': 'j', 'k': 'k', 'l': 'l', 'm': 'm', 'n': 'n', 'o': 'o', 'p': 'p', 'q': 'q', 'r': 'r', 's': 's', 't': 't', 'u': 'u', 'v': 'v', 'w': 'w', 'x': 'x', 'y': 'y', 'z': 'z'}
+        self.__key = {}
+        for i in range(26):
+            self.__key[i] = i
 
     def shuffle(self):
-        self.__letters = random.sample("abcdefghijklmnopqrstuvwxyz",k=2)
+        self.__letters = random.sample(range(26),k=2)
         self.__key[self.__letters[0]],self.__key[self.__letters[1]] = self.__key[self.__letters[1]],self.__key[self.__letters[0]]
 
 
@@ -15,7 +15,7 @@ class monoAlphabeticSubstitution():
         self.__key[self.__letters[0]],self.__key[self.__letters[1]] = self.__key[self.__letters[1]],self.__key[self.__letters[0]]
 
     def decipher(self):
-        plainText = ""
+        plainText = []
         for letter in self.__cipher:
-            plainText += self.__key[letter]
+            plainText.append(self.__key[letter])
         return plainText
