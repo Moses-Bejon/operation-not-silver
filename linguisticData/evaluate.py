d04d99fb54eea2f9aebb15c7d0925eb8330c634d
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-05-18 01:51:00 +0100

    Refactored evaluate.py
    Now takes inputs as lists of integers

--- linguisticData/evaluate.py
+++ linguisticData/evaluate.py
@@ -1,5 +1,5 @@
-from numpy import *
 import json
+from numpy import *
 
 # uses the angle between the two vectors of ideal frequencies and actual frequencies (the bigger the less similar)
 # also this seems to be pretty useful as it's very expensive to calculate and not very effective
@@ -8,7 +8,7 @@ with open("letterFrequenciesSorted.json","r") as file:
 def evaluateLetterFrequenciesSubstituted(plainText):
     letterFrequencies = zeros(26,dtype=uintc)
     for letter in plainText:
-        letterFrequencies[ord(letter)-97]+=1
+        letterFrequencies[letter]+=1
     letterFrequencies = sorted(letterFrequencies)
     return dot(letterFrequencies,idealLetterFrequenciesSorted)/(linalg.norm(letterFrequencies)*linalg.norm(idealLetterFrequenciesSorted))
 
@@ -17,35 +17,30 @@ with open("letterFrequenciesUnsorted.json","r") as file:
 def evaluateLetterFrequenciesUnsubstituted(plainText):
     letterFrequencies = zeros(26, dtype=uintc)
     for letter in plainText:
-        letterFrequencies[ord(letter) - 97] += 1
+        letterFrequencies[letter] += 1
     return dot(letterFrequencies, idealLetterFrequenciesUnsorted) / (
                 linalg.norm(letterFrequencies) * linalg.norm(idealLetterFrequenciesUnsorted))
 
 # a much better, and computationally cheaper, approach. Logged quadgram frequencies (the more negative, the less similar)
 with open("quadgram proportions.json","r") as file:
     idealQuadgramFrequencies = json.load(file)
-floor = -25 # this is the value when something appears 0 times in our ideal text (very negative because not very similar)
-# if something appears once it's about -21
 def evaluateQuadgramFrequencies(plainText):
-    window = plainText[:4]
+
+    currentHash = plainText[0]*17576+plainText[1]*676+plainText[2]*26+plainText[3]
+
     fitness = 0
-    for letter in plainText[4:]:
-        try:
-            fitness += idealQuadgramFrequencies[window]
-        except:
-            fitness += floor
-        window = window[1:] + letter
+    for i in range(len(plainText)-4):
+        first = plainText[i] * 17576
+        last = plainText[i+4]
+        fitness += idealQuadgramFrequencies[currentHash]
+        currentHash = 26*(currentHash-first)+last
     return fitness
 
 with open("bigram proportions.json","r") as file:
     idealBigramFrequencies = json.load(file)
 def evaluateBigramFrequencies(plainText):
-    window = plainText[:2]
+
     fitness = 0
-    for letter in plainText[2:]:
-        try:
-            fitness += idealBigramFrequencies[window]
-        except:
-            fitness += floor
-        window = window[1:] + letter
+    for i in range(len(plainText)-1):
+        fitness += idealBigramFrequencies[plainText[i]*26+plainText[i+1]]
     return fitness
