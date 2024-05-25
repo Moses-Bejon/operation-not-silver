116d3c2346dfa0e8b49cf7622cfd4f979f8bd81d
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-05-26 00:08:36 +0100

    Update evaluate.py
    added some functions that are useful for finding the period of a polyalphabetic substitution cipher

--- linguisticData/evaluate.py
+++ linguisticData/evaluate.py
@@ -1,18 +1,50 @@
 import json
 from numpy import *
+from math import log
 
-# uses the angle between the two vectors of ideal frequencies and actual frequencies (the bigger the less similar)
-# also this seems to be pretty useful as it's very expensive to calculate and not very effective
-with open("letterFrequenciesSorted.json","r") as file:
-    idealLetterFrequenciesSorted = array(json.load(file),dtype=uintc)
-def evaluateLetterFrequenciesSubstituted(plainText):
-    letterFrequencies = zeros(26,dtype=uintc)
+def getIOC(letterFrequencies,total):
+    return sum(letter*(letter-1) for letter in letterFrequencies)/(total*(total-1))
+
+# letter frequencies must be sorted and normalised
+with open("Text analysis/letterFrequenciesSortedNormalised.json","r") as file:
+    idealLetterFrequenciesSortedNormalised = json.load(file)
+def getTwist(letterFrequencies):
+    twist = 0
+    for i in range(13):
+        twist += idealLetterFrequenciesSortedNormalised[i + 13]-idealLetterFrequenciesSortedNormalised[i]+(letterFrequencies[i]-letterFrequencies[i + 13])
+    return twist
+
+# letter frequencies must be normalised
+def getEntropy(letterFrequencies):
+    entropy = 0
+    for frequency in letterFrequencies:
+        try:
+            entropy -= frequency*log(frequency,26)
+        except:
+            entropy += frequency*1.5
+    return entropy
+
+def getLetterFrequencies(plainText):
+    letterFrequencies = [0]*26
+    total = 0
     for letter in plainText:
-        letterFrequencies[letter]+=1
-    letterFrequencies = sorted(letterFrequencies)
+        letterFrequencies[letter] += 1
+        total += 1
+    return (letterFrequencies,total)
+
+def normaliseLetterFrequencies(letterFrequencies,total):
+    normalisedLetterFrequencies = []
+    for letter in letterFrequencies:
+        normalisedLetterFrequencies.append(letter/total)
+    return normalisedLetterFrequencies
+
+# letter frequencies must be sorted
+with open("Text analysis/letterFrequenciesSorted.json") as file:
+    idealLetterFrequenciesSorted = json.load(file)
+def getVectorEvaluationSubstituted(letterFrequencies):
     return dot(letterFrequencies,idealLetterFrequenciesSorted)/(linalg.norm(letterFrequencies)*linalg.norm(idealLetterFrequenciesSorted))
 
-with open("letterFrequenciesUnsorted.json","r") as file:
+with open("Text analysis/letterFrequenciesUnsorted.json","r") as file:
     idealLetterFrequenciesUnsorted = array(json.load(file),dtype=uintc)
 def evaluateLetterFrequenciesUnsubstituted(plainText):
     letterFrequencies = zeros(26, dtype=uintc)
@@ -22,7 +54,7 @@ def evaluateLetterFrequenciesUnsubstituted(plainText):
                 linalg.norm(letterFrequencies) * linalg.norm(idealLetterFrequenciesUnsorted))
 
 # a much better, and computationally cheaper, approach. Logged quadgram frequencies (the more negative, the less similar)
-with open("quadgram proportions.json","r") as file:
+with open("Text analysis/quadgram proportions.json","r") as file:
     idealQuadgramFrequencies = json.load(file)
 def evaluateQuadgramFrequencies(plainText):
 
@@ -36,7 +68,7 @@ def evaluateQuadgramFrequencies(plainText):
         currentHash = 26*(currentHash-first)+last
     return fitness
 
-with open("bigram proportions.json","r") as file:
+with open("Text analysis/bigram proportions.json","r") as file:
     idealBigramFrequencies = json.load(file)
 def evaluateBigramFrequencies(plainText):
 
