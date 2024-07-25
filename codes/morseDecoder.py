dc0a2eb1ba1d2ab68d52c4f2611e8afa4624108c
Crackerjack_404 <72654068+Future-Programmer-cryptographer@users.noreply.github.com>
2024-07-25 10:59:22 +0100

    Update morseDecoder.py

--- codes/morseDecoder.py
+++ codes/morseDecoder.py
@@ -1,1 +1,55 @@
+encodingTable = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    " ": "SPACE",
+}
 
+decodingTable = {a: k for k, a in encodingTable.items()}
+
+def encode(s):
+    enc = " ".join(encodingTable[x] for x in s)
+    return enc.replace(" SPACE ", "   ")
+
+def decode(encoded):
+    symbols = encoded.replace("   ", " SPACE ").split(" ")
+    return "".join(decodingTable[x] for x in symbols)
+
+cipher = input()
+print(decode(cipher))
