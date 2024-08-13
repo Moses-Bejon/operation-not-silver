071d68fe0c22806c4145124e5536f8c56c7f4c84
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-08-13 17:48:23 +0100

    columnar substitution now uses the permutationKey

--- transpositionCiphers/columnarSubstitution.py
+++ transpositionCiphers/columnarSubstitution.py
@@ -1,46 +1,36 @@
-import random
+from permutationKey import permutationKey
 
 class columnarSubstitution():
 
     def __init__(self,cipher):
-        self.__cipher = cipher # the cipherText
+        self.__cipher = cipher
         self.__length = len(self.__cipher)
 
-        self.__maxKeyLength = int(self.__length**0.5)
-        self.__setNewKeyLength(2)
+        self.__key = permutationKey(range(2,int(self.__length**0.5)+1))
+        self.updateKeyLength()
 
     def shuffle(self):
-        if random.random() > 0.5:
-            self.__rollAmount = False
-            self.__choices = random.sample(self.__key, k=2)
-            self.__key[self.__choices[0]], self.__key[self.__choices[1]] = self.__key[self.__choices[1]], self.__key[self.__choices[0]]
-        else:
-            self.__rollAmount = random.randint(1, self.__keyLength - 1)
-            self.__key = self.__key[self.__rollAmount:] + self.__key[:self.__rollAmount]
+        self.__key.shuffle()
 
     def shake(self):
-        if self.__keyLength <= self.__maxKeyLength:
-            self.__setNewKeyLength(self.__keyLength+1)
-        else:
-            self.__setNewKeyLength(2)
+        self.__key.shake()
+        self.updateKeyLength()
 
+    def updateKeyLength(self):
+        self.__keyLength = self.__key.getLength()
+        self.__numberOfFilledRows, self.__leftOver = divmod(self.__length, self.__keyLength)
 
     def undoShuffle(self):
-        if self.__rollAmount:
-            self.__key = self.__key[self.__keyLength - self.__rollAmount:] + self.__key[:self.__keyLength - self.__rollAmount]
-        else:
-            self.__key[self.__choices[0]], self.__key[self.__choices[1]] = self.__key[self.__choices[1]], self.__key[self.__choices[0]]
-    
-    def __setNewKeyLength(self,length):
-        self.__keyLength = length
-        self.__key = list(range(self.__keyLength))
-        self.__numberOfFilledRows, self.__leftOver = divmod(self.__length, self.__keyLength)
+        self.__key.undoShuffle()
+
     
     def decipher(self):
         # this decipher function is really efficient, at the cost of being really unreadable
         plainText = []
 
-        orderedLongs = self.__key[:self.__leftOver]
+        key = self.__key.getKey()
+
+        orderedLongs = key[:self.__leftOver]
 
         lengthLeadingUpTo = {0: 0}
         longNumbers = set(orderedLongs)
@@ -53,7 +43,7 @@ class columnarSubstitution():
             lengthLeadingUpTo[i] = place
 
         for row in range(self.__numberOfFilledRows):
-            for column in self.__key:
+            for column in key:
                 plainText.append(self.__cipher[lengthLeadingUpTo[column]+row])
 
         for column in orderedLongs:
