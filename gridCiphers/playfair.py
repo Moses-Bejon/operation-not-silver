7cac5e56ee9605e438530f54c1d6ecc8c2f257c7
Crackerjack_404 <72654068+Future-Programmer-cryptographer@users.noreply.github.com>
2024-08-23 19:00:40 +0100

    better playfair -still not 100% working

--- gridCiphers/playfair.py
+++ gridCiphers/playfair.py
@@ -13,7 +13,6 @@ def stringToInt(cipher):
             formattedCipher.append(ord(unidecode(letter).lower()) - 97)
     return [c if c < 9 else c - 1 for c in formattedCipher]
 
-
 def intToString(cipher):
     formattedCipher = ""
     for letter in cipher:
@@ -24,98 +23,134 @@ def intToString(cipher):
 
 class playfair:
     def __init__(self, cipherText):
-        self.cipherText = cipherText
-        self.key = self.generateKey()
+        self.__cipherText = cipherText
+        self.__key = self.generateKey()
 
     def generateKey(self):
         # exclude (ideally) letter j from alphabet - this is for 5x5 grid
         alphabet = [i for i in range(25)]
         random.shuffle(alphabet)
-        return alphabet
+        return [alphabet[i:i+5] for i in range(0,25,5)]
 
-    def decipher(self):
+    def decipher(self, key):
         plainText = []
-        for i in range(0, len(self.cipherText),2):
-            a = self.cipherText[i]
-            b = self.cipherText[i+1]
-            plainText.extend(self.decodePair(a,b))
+        for i in range(0, len(self.__cipherText), 2):
+            a = self.__cipherText[i]
+            b = self.__cipherText[i + 1]
+            plainText.extend(self.decodePair(a, b, key))
         return plainText
 
-    def decodePair(self, a, b):
-        rowA, colA = divmod(self.key.index(a), 5)
-        rowB, colB = divmod(self.key.index(b), 5)
+    def decodePair(self, a, b, key):
+        posA = [(ix, iy) for ix, row in enumerate(key) for iy, i in enumerate(row) if i == a][0]
+        posB = [(ix, iy) for ix, row in enumerate(key) for iy, i in enumerate(row) if i == b][0]
+
+        rowA, colA = posA
+        rowB, colB = posB
 
         if rowA == rowB:
-            return [(rowA * 5 + (colA - 1) % 5), (rowB * 5 + (colB - 1) % 5)]
+            # same row= shift columns left
+            new_a = key[rowA][(colA - 1) % 5]
+            new_b = key[rowB][(colB - 1) % 5]
         elif colA == colB:
-            return [((rowA-1)%5 * 5 + colA), ((rowB-1)%5 * 5 + colB)]
+            # same col= shift rows up
+            new_a = key[(rowA - 1) % 5][colA]
+            new_b = key[(rowB - 1) % 5][colB]
         else:
-            return [(rowA * 5 + colB), (rowB * 5 + colA)]
+            # swap columns but keep rows
+            new_a = key[rowA][colB]
+            new_b = key[rowB][colA]
 
+        return [new_a, new_b]
 
-    def shuffle(self):
+    def shuffle(self, key):
+        key = [row[:] for row in key]
         choice = random.randint(1, 6)
         if choice == 1:
-            self.swapElements()
+            self.swapElements(key)
         elif choice == 2:
-            self.swapRows()
+            self.swapRows(key)
         elif choice == 3:
-            self.swapColumns()
+            self.swapColumns(key)
         elif choice == 4:
-            self.flipDiagonal()
+            self.flipDiagonal(key)
         elif choice == 5:
-            # flip the square around the diagonal that runs from upper left to lower right
-            self.flipVertical()
+            self.flipVertical(key)
         elif choice == 6:
-            self.flipHorizontal()
+            self.flipHorizontal(key)
+        return key
+        # Debug print
+        # self.printKey()
 
-    def swapElements(self):
-        a,b = random.sample(range(25),2)
-        self.key[a], self.key[b] = self.key[b], self.key[a]
+    def swapElements(self, key):
+        a, b = random.sample(range(25), 2)
+        rowA, colA = divmod(a, 5)
+        rowB, colB = divmod(b, 5)
+        key[rowA][colA], key[rowB][colB] = key[rowB][colB], key[rowA][colA]
 
-    def swapRows(self):
-            rowA, rowB = random.sample(range(25), 2)
-            self.key[rowA * 5:rowA * 5 + 5], self.key[rowB * 5:rowB * 5 + 5] = self.key[rowB * 5:rowB * 5 + 5], self.key[rowA * 5:rowA * 5 + 5]
+    def swapRows(self, key):
+        rowA, rowB = random.sample(range(5), 2)
+        key[rowA], key[rowB] = key[rowB], key[rowA]
 
-    def swapColumns(self):
+    def swapColumns(self, key):
         colA, colB = random.sample(range(5), 2)
         for i in range(5):
-            self.key[i * 5 + colA], self.key[i * 5 + colB] = self.key[i * 5 + colB], self.key[i * 5 + colA]
+            key[i][colA], key[i][colB] = key[i][colB], key[i][colA]
 
-    def flipDiagonal(self):
-        self.key = [self.key[i%5 * 5+i //5] for i in range(25)]
+    def flipDiagonal(self, key):
+        key[:] = [[key[j][i] for j in range(5)] for i in range(5)]
 
-    def flipVertical(self):
+    def flipVertical(self, key):
+        key.reverse()
+
+    def flipHorizontal(self, key):
         for i in range(5):
-            self.key[i*5:(i+1) * 5] = self.key[i*5:(i+1) * 5][::-1]
+            key[i] = key[i][::-1]
 
-    def flipHorizontal(self):
-        self.key = self.key[::-1]
+    def printKey(self):
+        print("current Key:")
+        if self.__key:
+            for row in self.__key:
+                print(row)
 
-def evaluate(plaintext):
-    bigramScore = evaluateBigramFrequencies(plaintext)
-    quadgramScore = evaluateQuadgramFrequencies(plaintext)
-    letterFreq, _ = getLetterFrequencies(plaintext)
-    iocScore = getIOC(letterFreq, len(plaintext))
+    def evaluateDecryption(self, plainText):
+        bigramScore = evaluateBigramFrequencies(plainText)
+        quadgramScore = evaluateQuadgramFrequencies(plainText)
+        letterFreq, _ = getLetterFrequencies(plainText)
+        iocScore = getIOC(letterFreq, len(plainText))
 
-    totalScore = bigramScore + quadgramScore + 2 * iocScore
-    return totalScore
+        totalScore = bigramScore + quadgramScore + 2 * iocScore
+        return totalScore
+
+    def generateCandidateKey(self, currentKey):
+        candidateKey = self.shuffle(currentKey)
+        # debug print statement
+        # print(f'Shuffled key: {candidateKey}')
+        return candidateKey
+
+    def decrypt(self, key):
+        plainText = self.decipher(key)
+        # print(f'Decrypted text: {intToString(plainText)}')
+        return plainText
 
 
-cipherText = "UDSDAEEPVFHPKNNMPILPBMNGDOOGHPGDVFHIVQRSURBETIREAFHPAVKFREHRRMFANFPUDMRAAUPIAGPEXFTGRUODWRBNFNDOTGPWQGDMNLQVWEUWHGLDFSAUNOQPUALZSDZDGUFABEZDRBDFDVVQRSGMBEIZTDFNOPPLPUUVRBAUGTVEHRARFKDRBEODUDVEUCAWRBPRDSNEBXRSLTPWQGRAFAKGLPUWHGLZBFREIEREZLRETZWGYNLPDUFPECPZZDMUGUUTICGUIARAODOQGDUGIZGHUALZMUBVZDDMZDRBGUFAEFBRDMARDFOPBRPRNLOMSDSRXNOREBRADMGKANMHKIDZUMOAPLOAAFUNRZARSIGHUSMGZDRDEPUWBEIFREOPLSFNBWAUMPTLMGNLRZARSIPIAUXGZDPR"
+cipherText = "MDSOASOGTGKCDRBZEQVSKYMHFVIBDSKYMHCOROCEGODGABUICQMRORAOEAIHPEVFHPDMQCXCNDPUMRKBBPASZKGQPLABKENPNBVIQCASYQWBGZGUAEKYKBSHIQBUFSCPVLEQOEGUPBBNEQRFQYQCKSZGDCGUQSSIDCKGOGKRXZEQDKFVSAUCOCLNMRRCHWCMOBVFPDNBLVXCPEDRMHFVPDFVOVRCEAHRFSRLXCZMGQUQBXKGGSOBPUNPMDSHQBUIFNSGDUDUDCOWGSRFYTCYMRDSLTRDBXARZRQGKDQITVPLFVOIASDPQWQRDRXCPEGECRVFEDPLCDSDMCBAIQDQPLCOBNVBOZURBYXCNURQBXNQWSEKQUTCIQAELTFICZEQSHOGHWGENLTMTCPLEKBAUNAEOW"
 
 cipherText = stringToInt(cipherText)
-playfair = playfair(cipherText)
+playfairCipher = playfair(cipherText)
 
-key, decryption = simulatedAnnealing(
-    playfair.key,
-    playfair.shuffle,
-    lambda dec: evaluate(playfair.decipher()),
-    playfair.decipher
+
+initialKey = playfairCipher.generateKey()
+bestKey, bestDecryption = simulatedAnnealing(
+    initialKey=initialKey,
+    generateCandidateKey=playfairCipher.generateCandidateKey,
+    evaluateFitness=lambda dec: playfairCipher.evaluateDecryption(dec),
+    decrypt=lambda key: playfairCipher.decrypt(key),
+    maxIter=20000,
+    coolingRate=0.999
 )
 
-print(f'Best key: {key}')
-print(f'Best decrypt: {intToString(decryption)}')
+print(f'Best key: {bestKey}')
+print(f'Best decryption: {intToString(bestDecryption)}')
 
 
 
