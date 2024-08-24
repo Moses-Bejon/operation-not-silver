3ba4a6b7e00593619cfae5bef59f50df0cc8e768
Crackerjack_404 <72654068+Future-Programmer-cryptographer@users.noreply.github.com>
2024-08-24 20:12:50 +0100

    Now works with known key and integer

--- gridCiphers/playfair.py
+++ gridCiphers/playfair.py
@@ -2,7 +2,7 @@ import random
 from unidecode import unidecode
 from evaluate import getIOC, getLetterFrequencies, evaluateBigramFrequencies, evaluateQuadgramFrequencies
 from simulatedAnnealing import simulatedAnnealing
-from formatCipher import intToString
+from copy import deepcopy
 
 # need to swap j for i
 def stringToInt(cipher):
@@ -14,18 +14,70 @@ def stringToInt(cipher):
             formattedCipher.append(ord(unidecode(letter).lower()) - 97)
     return [c if c < 9 else c - 1 for c in formattedCipher]
 
-# def intToString(cipher):
-#     formattedCipher = ""
-#     for letter in cipher:
-#         if letter >= 9:
-#             letter += 1  # adjust to skip 'j'
-#         formattedCipher += chr(letter + 97)
-#     return formattedCipher
+def intToString(cipher):
+    formattedCipher = ""
+    for letter in cipher:
+        if letter >= 9:
+            letter += 1  # adjust to skip 'j'
+        formattedCipher += chr(letter + 97)
+    return formattedCipher
+
+def createDigraphs(cipher):
+    digraphs = []
+    i = 0
+    while i < len(cipher):
+        a = cipher[i]
+        if i + 1 < len(cipher):
+            b = cipher[i + 1]
+            if a == b:
+                digraphs.append([a, 23])  # 'x' corresponds to 23
+                i += 1
+            else:
+                digraphs.append([a, b])
+                i += 2
+        else:
+            digraphs.append([a, 23])  # 'x' corresponds to 23
+            i += 1
+    return digraphs
 
 class playfair:
-    def __init__(self, cipherText):
+    def __init__(self, cipherText, key=None):
         self.__cipherText = cipherText
-        self.__key = self.generateKey()
+        if key is None:
+            self.__key = self.generateKey()
+            self.__keySquare = self.__key
+        else:
+            if isinstance(key, str):
+                self.__keySquare = self.createSquare(key)
+            else:
+                self.__keySquare = key
+
+    def createSquare(self, key):
+        keySquare = []
+        usedNumbers = set()
+
+        for letter in key:
+            num = ord(letter) - 97
+            if num == 9:  # Skip 'j'
+                continue
+            num = num if num < 9 else num - 1  # Adjust for 'j'
+            if num not in usedNumbers:
+                keySquare.append(num)
+                usedNumbers.add(num)
+
+        for num in range(25):
+            if num not in usedNumbers:
+                keySquare.append(num)
+                usedNumbers.add(num)
+
+        # Return the 5x5 key square
+        return [keySquare[i * 5:(i + 1) * 5] for i in range(5)]
+
+    def findPosition(self, num):
+        for row in range(5):
+            for col in range(5):
+                if self.__keySquare[row][col] == num:
+                    return row, col
 
     def generateKey(self):
         # exclude (ideally) letter j from alphabet - this is for 5x5 grid
@@ -33,48 +85,43 @@ class playfair:
         random.shuffle(alphabet)
         return [alphabet[i:i+5] for i in range(0,25,5)]
 
-    def decipher(self, key):
+    # def decipher(self, key):
+    #     plainText = []
+    #     for i in range(0, len(self.__cipherText), 2):
+    #         a = self.__cipherText[i]
+    #         b = self.__cipherText[i + 1]
+    #         decodedPair = self.decodePair(a, b, key)
+    #         plainText.extend(decodedPair)
+    #
+    #     return plainText
+
+    def decipher(self):
+        digraphs = createDigraphs(self.__cipherText)
         plainText = []
-        for i in range(0, len(self.__cipherText), 2):
-            a = self.__cipherText[i]
-            b = self.__cipherText[i + 1]
-            decodedPair = self.decodePair(a, b, key)
-            plainText.extend(decodedPair)
+        for pair in digraphs:
+            plainText.extend(self.decodePair(pair))
 
         return plainText
 
-    def decodePair(self, a, b, key):
+    def decodePair(self, digraph):
         # convert 9 to 8
-        convertedA, convertedB = [8 if x == 9 else x for x in [a, b]]
+        a,b = digraph
 
-        # find positions in the grid
-        posA = [(ix, iy) for ix, row in enumerate(key) for iy, i in enumerate(row) if i == convertedA][0]
-        posB = [(ix, iy) for ix, row in enumerate(key) for iy, i in enumerate(row) if i == convertedB][0]
-
-        rowA, colA = posA
-        rowB, colB = posB
+        rowA, colA = self.findPosition(a)
+        rowB, colB = self.findPosition(b)
 
         if rowA == rowB:
-            # same row= shift columns left
-            newA = key[rowA][(colA - 1) % 5]
-            newB = key[rowB][(colB - 1) % 5]
+            # Same row: shift columns left
+            return [self.__keySquare[rowA][(colA - 1) % 5], self.__keySquare[rowB][(colB - 1) % 5]]
         elif colA == colB:
-            # same col= shift rows up
-            newA = key[(rowA - 1) % 5][colA]
-            newB = key[(rowB - 1) % 5][colB]
+            # Same column: shift rows up
+            return [self.__keySquare[(rowA - 1) % 5][colA], self.__keySquare[(rowB - 1) % 5][colB]]
         else:
-            # swap columns but keep rows
-            newA = key[rowA][colB]
-            newB = key[rowB][colA]
-
-        # convert back to pass into evaluator
-        return [
-            newA if newA != 8 else 9,
-            newB if newB != 8 else 9
-        ]
+            # Rectangle: swap columns
+            return [self.__keySquare[rowA][colB], self.__keySquare[rowB][colA]]
 
     def shuffle(self, key):
-        key = [row[:] for row in key]
+        key = deepcopy(key)
         choice = random.randint(1, 100)
         if choice <= 90:
             self.swapElements(key)
@@ -141,31 +188,44 @@ class playfair:
         # print(f'Shuffled key: {candidateKey}')
         return candidateKey
 
-    def decrypt(self, key):
-        plainText = self.decipher(key)
-        # print(f'Decrypted text: {intToString(plainText)}')
-        return plainText
+    # def decrypt(self, key):
+    #     plainText = self.decipher(key)
+    #     # print(f'Decrypted text: {intToString(plainText)}')
+    #     return plainText
+
 
 
 cipherText = "MDSOASOGTGKCDRBZEQVSKYMHFVIBDSKYMHCOROCEGODGABUICQMRORAOEAIHPEVFHPDMQCXCNDPUMRKBBPASZKGQPLABKENPNBVIQCASYQWBGZGUAEKYKBSHIQBUFSCPVLEQOEGUPBBNEQRFQYQCKSZGDCGUQSSIDCKGOGKRXZEQDKFVSAUCOCLNMRRCHWCMOBVFPDNBLVXCPEDRMHFVPDFVOVRCEAHRFSRLXCZMGQUQBXKGGSOBPUNPMDSHQBUIFNSGDUDUDCOWGSRFYTCYMRDSLTRDBXARZRQGKDQITVPLFVOIASDPQWQRDRXCPEGECRVFEDPLCDSDMCBAIQDQPLCOBNVBOZURBYXCNURQBXNQWSEKQUTCIQAELTFICZEQSHOGHWGENLTMTCPLEKBAUNAEOW"
 
 cipherText = stringToInt(cipherText)
+
+# # known key
+# key = "GROCEISABDFHKLMNPQTUVWXYZ"
+# key = key.lower()
+# 
+# playfairCipher = playfair(cipherText, key)
+# 
+# print(f'integer cipher: {cipherText}')
+# decrypt = playfairCipher.decipher()
+# decrypt = intToString(decrypt)
+# decrypt = decrypt.replace('x', '')
+# print(f'Decryption: {decrypt}')
+
+
+# unknown key 
 playfairCipher = playfair(cipherText)
-
-
 initialKey = playfairCipher.generateKey()
 bestKey, bestDecryption = simulatedAnnealing(
     initialKey=initialKey,
     generateCandidateKey=playfairCipher.generateCandidateKey,
     evaluateFitness=lambda dec: playfairCipher.evaluateDecryption(dec),
-    decrypt=lambda key: playfairCipher.decrypt(key),
-    maxIter=100000,
-    coolingRate=0.9999,
+    decrypt=lambda key: playfairCipher.decipher(),
+    maxIter=10000,
+    coolingRate=0.999,
     initialTemp=100.0
 )
 
 print(f'Best key: {bestKey}')
 print(f'Best decryption: {intToString(bestDecryption)}')
-
 
 
