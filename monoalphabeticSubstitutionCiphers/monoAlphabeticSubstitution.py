17a7c1d2859cd49caeb2e63e7195293aec1dacc4
Moses-Bejon <idontwantnumbersinmyname123@gmail.com>
2024-12-16 14:48:49 +0000

    Made compatible with wheatstone

--- monoalphabeticSubstitutionCiphers/monoAlphabeticSubstitution.py
+++ monoalphabeticSubstitutionCiphers/monoAlphabeticSubstitution.py
@@ -2,20 +2,31 @@ import random
 class monoAlphabeticSubstitution():
     def __init__(self,cipher):
         self.__cipher = cipher
-        self.__key = {}
+        self._key = {}
         for i in range(26):
-            self.__key[i] = i
+            self._key[i] = i
+
+    def printKey(self):
+        print(self._key)
+
+    def getKey(self):
+        return self._key
+
+    def getNumberOfKeys(self):
+        return 1
+
+    def injectKey(self,key):
+        self._key = key
 
     def shuffle(self):
-        self.__letters = random.sample(range(26),k=2)
-        self.__key[self.__letters[0]],self.__key[self.__letters[1]] = self.__key[self.__letters[1]],self.__key[self.__letters[0]]
-
+        self._letters = random.sample(range(26), k=2)
+        self._key[self._letters[0]],self._key[self._letters[1]] = self._key[self._letters[1]],self._key[self._letters[0]]
 
     def undoShuffle(self):
-        self.__key[self.__letters[0]],self.__key[self.__letters[1]] = self.__key[self.__letters[1]],self.__key[self.__letters[0]]
+        self._key[self._letters[0]],self._key[self._letters[1]] = self._key[self._letters[1]],self._key[self._letters[0]]
 
     def decipher(self):
         plainText = []
         for letter in self.__cipher:
-            plainText.append(self.__key[letter])
+            plainText.append(self._key[letter])
         return plainText
