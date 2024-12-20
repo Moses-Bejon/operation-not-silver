9e21bbb9dc14998a051fa25275c14c5fd93324ba
Moses-Bejon <idontwantnumbersinmyname123@gmail.com>
2024-12-20 16:43:19 +0000

    Delete Current progress on Solitaire for Andrew directory

--- Current progress on Solitaire for Andrew/Text analysis/checkAddsToOne.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("letterFrequenciesSortedNormalised.json","r") as proportions:
-    data = json.load(proportions)
-
-total = 0
-for datum in data:
-    total += datum
-print(total)
--- Current progress on Solitaire for Andrew/Text analysis/editData.py
+++
@@ -1,7 +0,0 @@
-with open("../../Text analysis/trainingData", "r") as trainingData:
-    trainingData = trainingData.readlines()
-
-trainingData = trainingData[:107270]+trainingData[119092:]
-
-with open("../../Text analysis/trainingData", "w") as file:
-    file.writelines(trainingData)
